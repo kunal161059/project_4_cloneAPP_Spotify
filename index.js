28961b349832bc2a3b119e3d3a03456017947fea
Kunal Raghuwanshi <kunalraghu1c001@gmail.com>
2024-06-01 23:58:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ const playMusic = (track, pause = false) => {
 }
 
 async function displayAlbums() {
-    let a = await fetch(`http://127.0.0.1:3000/songs/`)
+    let a = await fetch(`songs/`)
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -91,7 +91,7 @@ async function displayAlbums() {
 
         if (e.href.includes("/songs")) {
             let folder = e.href.split("/").slice(-2)[0]
-            let a = await fetch(`http://127.0.0.1:3000/songs/${folder}/info.json`)
+            let a = await fetch(`${folder}/info.json`)
             let response = await a.json();
 
             cardContainer.innerHTML = cardContainer.innerHTML + `
